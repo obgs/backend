5da485a3ff0889c7fc872b536d28104f608ec58c
flungloaf <flungloaf@gmail.com>
2020-12-13 21:50:15 +0300

    Use a stricter method to indetify whether changes have been made
    Co-authored-by: MOZGIII <mike-n@narod.ru>

--- .github/scripts/check-generate.sh
+++ .github/scripts/check-generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-set -euxo pipefail
+set -euo pipefail
+set -x
 
 go generate ./...
 
-if [[ $(git --no-pager diff) ]] ; then
-    echo "you need to run "go generate ./..." and commit the changes"
-    git --no-pager diff
+if [[ "$(git status --porcelain=v2 | wc -l)" -ne 0 ]] ; then
+    echo 'You need to run "go generate ./..." and commit the changes!'
+    git status --verbose --short
     exit 1
 fi
