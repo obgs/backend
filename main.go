01ee10b2ab0c7c097bb853013c1c75603055f486
Rifat Kudriakov <flungloaf@gmail.com>
2021-01-06 11:33:07 +0300

    Add a default graphql server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/open-boardgame-stats/backend/graphql/generated"
+	"github.com/open-boardgame-stats/backend/graphql/resolver"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
+		Resolvers: &resolver.Resolver{},
+	}))
+
+	http.Handle("/", playground.Handler("OBGS", "/graphql"))
+	http.Handle("/graphql", srv)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
