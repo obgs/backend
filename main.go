d49a1fed8833360d6fd09759f2364673485c861e
flungloaf <flungloaf@gmail.com>
2021-01-07 23:04:31 +0300

    Use cobra to manage CLI commands

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-
-	"github.com/open-boardgame-stats/backend/graphql/generated"
-	"github.com/open-boardgame-stats/backend/graphql/resolver"
-)
+import "github.com/open-boardgame-stats/backend/cmd"
 
 func main() {
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
-		Resolvers: &resolver.Resolver{},
-	}))
-
-	http.Handle("/", playground.Handler("OBGS", "/graphql"))
-	http.Handle("/graphql", srv)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	cmd.Execute()
 }
