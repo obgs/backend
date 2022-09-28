01c4ed2458ec7c74ad79e8a5e6efbbb056047c9d
flungloaf <flungloaf@gmail.com>
2022-09-28 15:32:27 +0300

    Create storage bucket on startup

--- filestorage/filestorage.go
+++ filestorage/filestorage.go
@@ -2,20 +2,27 @@ package filestorage
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	miniocreds "github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 type FileStorageService struct {
-	client *s3.PresignClient
-	bucket string
+	s3Client        *s3.Client
+	s3PresignClient *s3.PresignClient
+	bucket          string
+	usingMinio      bool
+	minioClient     *minio.Client
 }
 
 // NewFileStorageService creates a new FileStorageService
-func NewFileStorageService(accessKeyID, secretAccessKey, region, endpoint, bucket string, usingMinio bool) *FileStorageService {
+func NewFileStorageService(accessKeyID, secretAccessKey, region, endpoint, bucket string, usingMinio bool) (*FileStorageService, error) {
 	cfg := aws.Config{
 		Credentials: credentials.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, ""),
 		Region:      region,
@@ -26,21 +33,34 @@ func NewFileStorageService(accessKeyID, secretAccessKey, region, endpoint, bucke
 			}, nil
 		}),
 	}
-	client := s3.NewPresignClient(s3.NewFromConfig(cfg, func(o *s3.Options) {
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
 		if usingMinio {
 			o.UsePathStyle = true
 		}
-	}))
-	return &FileStorageService{
-		client,
-		bucket,
+	})
+	s3PresignClient := s3.NewPresignClient(s3Client)
+	var minioEndpoint = endpoint
+	minioEndpoint = strings.TrimPrefix(strings.TrimPrefix(minioEndpoint, "http://"), "https://")
+	minioClient, err := minio.New(minioEndpoint, &minio.Options{
+		Creds:  miniocreds.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: false,
+	})
+	if err != nil {
+		return nil, err
 	}
+	return &FileStorageService{
+		s3Client,
+		s3PresignClient,
+		bucket,
+		usingMinio,
+		minioClient,
+	}, nil
 }
 
 // SignUploadURL signs a URL for uploading a file to S3
 func (s *FileStorageService) SignUploadURL(ctx context.Context) (string, error) {
 	key := uuid.New()
-	req, err := s.client.PresignPutObject(ctx, &s3.PutObjectInput{
+	req, err := s.s3PresignClient.PresignPutObject(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(key.String()),
 	})
@@ -48,4 +68,48 @@ func (s *FileStorageService) SignUploadURL(ctx context.Context) (string, error) 
 		return "", err
 	}
 	return req.URL, nil
+}
+
+func (s *FileStorageService) CreateBucket(ctx context.Context) error {
+	resp, err := s.s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		return err
+	}
+	for _, bucket := range resp.Buckets {
+		if *bucket.Name == s.bucket {
+			return nil
+		}
+	}
+	if s.usingMinio {
+		err = s.minioClient.MakeBucket(ctx, s.bucket, minio.MakeBucketOptions{})
+		if err != nil {
+			return err
+		}
+		err = s.minioClient.SetBucketPolicy(ctx, s.bucket, `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:GetBucketLocation","s3:ListBucket"],"Resource":["arn:aws:s3:::`+s.bucket+`"]},{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:GetObject"],"Resource":["arn:aws:s3:::`+s.bucket+`/*"]}]} `)
+	} else {
+		_, err = s.s3Client.CreateBucket(ctx, &s3.CreateBucketInput{
+			Bucket: aws.String(s.bucket),
+			ACL:    types.BucketCannedACLPublicRead,
+		})
+
+		if err != nil {
+			return err
+		}
+		_, err = s.s3Client.PutBucketCors(ctx, &s3.PutBucketCorsInput{
+			Bucket: aws.String(s.bucket),
+			CORSConfiguration: &types.CORSConfiguration{
+				CORSRules: []types.CORSRule{
+					{
+						AllowedHeaders: []string{"*"},
+						AllowedMethods: []string{"GET", "PUT", "POST", "DELETE"},
+						AllowedOrigins: []string{"*"},
+						ExposeHeaders:  []string{"ETag"},
+						MaxAgeSeconds:  3000,
+					},
+				},
+			},
+		})
+	}
+
+	return err
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/lib/pq v1.10.6
+	github.com/minio/minio-go/v7 v7.0.37
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
 	github.com/vektah/gqlparser/v2 v2.4.7
@@ -37,6 +38,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.16 // indirect
 	github.com/aws/smithy-go v1.13.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
@@ -46,12 +48,21 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.3 // indirect
+	github.com/rs/xid v1.4.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -62,6 +73,7 @@ require (
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220817070843-5a390386f1f2 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.13-0.20220804200503-81c7dc4e4efa // indirect
