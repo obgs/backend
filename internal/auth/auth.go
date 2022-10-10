70f0029661907e2113b0874f698e2dbff1eed2a5
flungloaf <flungloaf@gmail.com>
2022-10-10 09:06:44 +0600

    Create the user's player when registering

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -91,29 +91,37 @@ func (a *AuthService) generateTokens(w http.ResponseWriter, userId uuid.UUID, st
 	})
 }
 
+func (a *AuthService) createUser(ctx context.Context, email, password string) (*ent.User, error) {
+	// hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	// create the user
+	u, err := a.client.User.Create().SetEmail(email).SetPassword(string(hashedPassword)).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %v", err)
+	}
+
+	// create the user's player
+	_, err = a.client.Player.Create().SetOwner(u).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create the user's player: %v", err)
+	}
+
+	return u, nil
+}
+
 // SignUp creates a new user with email and password from form data
 func (a *AuthService) SignUp(w http.ResponseWriter, r *http.Request) {
 	// get form data
 	email := r.FormValue("email")
 	password := r.FormValue("password")
 
-	// encrypt the password
-	hashedPassword, err := a.encryptPassword(password)
+	u, err := a.createUser(context.Background(), email, password)
 	if err != nil {
-		internalServerError(w, fmt.Sprintf("failed to hash password: %v", err))
-		return
-	}
-
-	ctx := context.Background()
-	// create the user
-	u, err := a.client.User.Create().SetEmail(email).SetPassword(string(hashedPassword)).Save(ctx)
-	if err != nil {
-		if ent.IsConstraintError(err) {
-			http.Error(w, fmt.Sprintf("user with email %s already exists", email), http.StatusConflict)
-			return
-		}
-		internalServerError(w, fmt.Sprintf("failed to create user: %v", err))
-
+		internalServerError(w, err.Error())
 		return
 	}
 
@@ -184,19 +192,9 @@ func (a *AuthService) oAuthSignUp(data oAuthData, w http.ResponseWriter) {
 	ctx := context.Background()
 	u, findErr := a.client.User.Query().Where(user.EmailEQ(email)).Only(ctx)
 	if ent.IsNotFound(findErr) {
-		hashedPassword, passErr := a.encryptPassword(a.randomPassword(RANDOM_PASSWORD_LENGTH))
-		if passErr != nil {
-			internalServerError(w, fmt.Sprintf("failed to hash password: %v", passErr))
-			return
-		}
-
-		newU, createErr := a.client.User.Create().
-			SetEmail(email).
-			SetPassword(string(hashedPassword)).
-			SetName(data.GetName()).
-			Save(ctx)
-		if createErr != nil {
-			internalServerError(w, fmt.Sprintf("failed to create user: %v", createErr))
+		newU, err := a.createUser(ctx, email, data.GetName())
+		if err != nil {
+			internalServerError(w, err.Error())
 			return
 		}
 
