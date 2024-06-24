9ebf0114ac0b0b28f292d6e34ae56210b8853908
Rifat Kudriakov <flungloaf@gmail.com>
2024-06-24 23:57:30 +0400

    Fix migration

--- internal/ent/migrate/migrations/20230602082148_game_versions_final.sql
+++ internal/ent/migrate/migrations/20230602082148_game_versions_final.sql
@@ -1,5 +1,5 @@
-ADD CONSTRAINT "matches_game_versions_matches" FOREIGN KEY ("game_version_matches") REFERENCES "game_versions" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "matches" ADD CONSTRAINT "matches_game_versions_matches" FOREIGN KEY ("game_version_matches") REFERENCES "game_versions" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-DROP TABLE stat_description_game;
+DROP TABLE "stat_description_game";
 
-ALTER TABLE stat_descriptions DROP COLUMN game_matches, ALTER COLUMN game_versions_matches DROP DEFAULT;
+ALTER TABLE "matches" DROP COLUMN "game_matches", ALTER COLUMN "game_version_matches" DROP DEFAULT;
