6e32303c68e34e6ae1b664d61d647b0db84219ae
Rifat Kudriakov <flungloaf@gmail.com>
2024-06-24 23:58:04 +0400

    Update seeds to correctly calculate aggregate stats

--- internal/ent/migrate/seed/matches.go
+++ internal/ent/migrate/seed/matches.go
@@ -13,6 +13,98 @@ import (
 
 const MATCH_MAX_NUMERIC_VALUE = 100
 
+func createPlainStats(
+	tx *ent.Tx,
+	descriptions []*ent.StatDescription,
+	player *ent.Player,
+	match *ent.Match,
+	values map[string]string,
+) ([]*ent.StatisticCreate, []*ent.StatDescription) {
+	stats := make([]*ent.StatisticCreate, 0, len(descriptions))
+	aggregateStatDescriptions := make([]*ent.StatDescription, 0, len(descriptions))
+	for _, d := range descriptions {
+		var value string
+		metadata, err := stat.UnmarshalMetadata(d.Type, d.Metadata)
+		if err != nil {
+			panic(err)
+		}
+
+		switch d.Type {
+		case stat.Enum:
+			enumMetadata, ok := metadata.(*stat.EnumStatMetadata)
+			if !ok {
+				panic("invalid metadata")
+			}
+			value = enumMetadata.PossibleValues[rand.Intn(len(enumMetadata.PossibleValues))]
+		case stat.Numeric:
+			value = strconv.Itoa(rand.Intn(MATCH_MAX_NUMERIC_VALUE))
+		case stat.Aggregate:
+			// aggregate stats should be processed after basic ones are all done
+			aggregateStatDescriptions = append(aggregateStatDescriptions, d)
+			continue
+		default:
+			panic("invalid stat type")
+		}
+
+		values[d.ID.String()] = value
+		stats = append(stats, tx.Statistic.Create().
+			SetValue(value).
+			SetPlayer(player).
+			SetStatDescription(d).
+			SetMatch(match))
+	}
+
+	return stats, aggregateStatDescriptions
+}
+
+func createAggregateStats(
+	tx *ent.Tx,
+	aggregateStats []*ent.StatDescription,
+	player *ent.Player,
+	match *ent.Match,
+	values map[string]string,
+) []*ent.StatisticCreate {
+	stats := make([]*ent.StatisticCreate, 0, len(aggregateStats))
+	for _, d := range aggregateStats {
+		var value string
+		metadata, err := stat.UnmarshalMetadata(d.Type, d.Metadata)
+		if err != nil {
+			panic(err)
+		}
+
+		switch d.Type {
+		case stat.Aggregate:
+			aggregateMetadata, ok := metadata.(*stat.AggregateStatMetadata)
+			if !ok {
+				panic("invalid metadata")
+			}
+			// other aggregate stats are not supported yet
+			if aggregateMetadata.Type != stat.AggregateSum {
+				panic("invalid aggregate metadata type")
+			}
+			sum := 0
+			for _, id := range aggregateMetadata.StatIds {
+				v, err := strconv.Atoi(values[id.String()])
+				if err != nil {
+					panic(err)
+				}
+				sum += v
+			}
+			value = strconv.Itoa(sum)
+		default:
+			panic("invalid stat type")
+		}
+
+		stats = append(stats, tx.Statistic.Create().
+			SetValue(value).
+			SetPlayer(player).
+			SetStatDescription(d).
+			SetMatch(match))
+	}
+
+	return stats
+}
+
 func createMatch(ctx context.Context, tx *ent.Tx, g *ent.GameVersion, players []*ent.Player) *ent.Match {
 	descriptions := tx.StatDescription.Query().
 		Where(
@@ -20,7 +112,7 @@ func createMatch(ctx context.Context, tx *ent.Tx, g *ent.GameVersion, players []
 		).
 		AllX(ctx)
 
-	stats := make([]*ent.Statistic, len(descriptions)*len(players))
+	stats := make([]*ent.StatisticCreate, 0, len(descriptions)*len(players))
 
 	match := tx.Match.Create().
 		SetGameVersion(g).
@@ -28,33 +120,13 @@ func createMatch(ctx context.Context, tx *ent.Tx, g *ent.GameVersion, players []
 		SaveX(ctx)
 
 	for _, player := range players {
-		for _, d := range descriptions {
-			var value string
-			metadata, err := stat.UnmarshalMetadata(d.Type, d.Metadata)
-			if err != nil {
-				panic(err)
-			}
-
-			switch d.Type {
-			case stat.Enum:
-				enumMetadata, ok := metadata.(*stat.EnumStatMetadata)
-				if !ok {
-					panic("invalid metadata")
-				}
-				value = enumMetadata.PossibleValues[0]
-			case stat.Numeric:
-				value = strconv.Itoa(rand.Intn(MATCH_MAX_NUMERIC_VALUE))
-			}
-
-			stats = append(stats, tx.Statistic.Create().
-				SetValue(value).
-				SetPlayer(player).
-				SetStatDescription(d).
-				SetMatch(match).
-				SaveX(ctx),
-			)
-		}
+		values := make(map[string]string, len(descriptions))
+		plainStats, aggregateStatDescriptions := createPlainStats(tx, descriptions, player, match, values)
+		aggregateStats := createAggregateStats(tx, aggregateStatDescriptions, player, match, values)
+		stats = append(stats, plainStats...)
+		stats = append(stats, aggregateStats...)
 	}
+	tx.Statistic.CreateBulk(stats...).SaveX(ctx)
 
 	return match
 }
